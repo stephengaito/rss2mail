fb99af5ca70b9b55cb6691450fa499f9779dc76d
Stephen Gaito <stephen@perceptisys.co.uk>
2013-12-02 17:42:51 +0000

    initial work on adding direct SMTP and LMTP transport (on local machines)

---
+++ lib/rss2mail/feed_lmtp.rb
@@ -0,0 +1,75 @@
+#--
+###############################################################################
+#                                                                             #
+# feed_lmtp is a component of rss2mail, the RSS to e-mail forwarder.          #
+#                                                                             #
+# Copyright (C) 2007-2013 Stephen Gaito                                       #
+#                                                                             #
+# Authors:                                                                    #
+#     Stephen Gaito <stephen@perceptisys.co.uk>                               #
+#                                                                             #
+# rss2mail is free software; you can redistribute it and/or modify it under   #
+# the terms of the GNU Affero General Public License as published by the Free #
+# Software Foundation; either version 3 of the License, or (at your option)   #
+# any later version.                                                          #
+#                                                                             #
+# rss2mail is distributed in the hope that it will be useful, but WITHOUT ANY #
+# WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS   #
+# FOR A PARTICULAR PURPOSE. See the GNU Affero General Public License for     #
+# more details.                                                               #
+#                                                                             #
+# You should have received a copy of the GNU Affero General Public License    #
+# along with rss2mail. If not, see <http://www.gnu.org/licenses/>.            #
+#                                                                             #
+###############################################################################
+#++
+
+# see: http://en.wikipedia.org/wiki/Local_Mail_Transfer_Protocol
+
+require 'rss2mail/feed_smtp'
+require 'net/smtp'
+
+# This is a very simple overlay of LMTP on top of Ruby's existing SMTP.
+#
+# This WILL NOT work for messages being sent to more than one recipient 
+# on the local machine.  But for our purposes, this is more than 
+# sufficient.
+
+module Net
+  class LMTP < SMTP
+
+    # Send LMTP's LHLO command instead of SMTP's HELO command
+    def helo(domain)
+      getok("LHLO #{domain}")
+    end
+
+    # Send LMTP's LHLO command instead of ESMTP's EHLO command
+    def ehlo(domain)
+      getok("LHLO #{domain}")
+    end
+  end
+end
+
+module RSS2Mail
+
+  class FeedLMTP < Feed
+
+    def send_mail(type_header, to, title, subject, body)
+      return if debug
+
+      to.each { | aRecipient |
+        Net::LMTP.start('localhost', 24) { |lmtp|
+          lmtp.send_message(build_message(type_header, title, subject, [aRecipient]), 
+                            FROM, aRecipient)
+        }
+      }
+
+      yield if block_given?
+    rescue Errno::EPIPE, IOError => err
+      error err, 'while sending mail using LMTP', cmd
+    end
+
+  end
+
+end
+
---
+++ lib/rss2mail/feed_smtp.rb
@@ -0,0 +1,71 @@
+#--
+###############################################################################
+#                                                                             #
+# feed_smtp is a component of rss2mail, the RSS to e-mail forwarder.          #
+#                                                                             #
+# Copyright (C) 2007-2013 Stephen Gaito                                       #
+#                                                                             #
+# Authors:                                                                    #
+#     Stephen Gaito <stephen@perceptisys.co.uk>                               #
+#                                                                             #
+# rss2mail is free software; you can redistribute it and/or modify it under   #
+# the terms of the GNU Affero General Public License as published by the Free #
+# Software Foundation; either version 3 of the License, or (at your option)   #
+# any later version.                                                          #
+#                                                                             #
+# rss2mail is distributed in the hope that it will be useful, but WITHOUT ANY #
+# WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS   #
+# FOR A PARTICULAR PURPOSE. See the GNU Affero General Public License for     #
+# more details.                                                               #
+#                                                                             #
+# You should have received a copy of the GNU Affero General Public License    #
+# along with rss2mail. If not, see <http://www.gnu.org/licenses/>.            #
+#                                                                             #
+###############################################################################
+#++
+
+# see: http://ruby-doc.org/stdlib-2.0/libdoc/net/smtp/rdoc/Net/SMTP.html
+# see: http://notepad.onghu.com/2007/3/26/sending-email-with-ruby
+
+require 'rss2mail/feed'
+require 'net/smtp'
+require 'date'
+require 'securerandom'
+
+module RSS2Mail
+
+  class FeedSMTP < Feed
+
+    MAIL = "not used"
+
+    def build_message(type_header, title, subject, to)
+      msgstr = <<END_OF_MESSAGE
+From: #{FROM}
+To: #{to.join(', ')}
+Subject: [#{title}] #{subject}
+Date: #{DateTime.now.strftime("%a, %d %b %Y %H:%M:%S %z")}
+Message-Id: #{SecureRandom.uuid}
+#{type_header}
+
+#{body}
+END_OF_MESSAGE
+      msgstr
+    end
+
+    def send_mail(type_header, to, title, subject, body)
+      return if debug
+
+      Net::SMTP.start('localhost', 25) { |smtp|
+        smtp.send_message(build_message(type_header, title, subject, *to), 
+                          FROM, *to)
+      }
+
+      yield if block_given?
+    rescue Errno::EPIPE, IOError => err
+      error err, 'while sending mail using SMTP', cmd
+    end
+
+  end
+
+end
+
